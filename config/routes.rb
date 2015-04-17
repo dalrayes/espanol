107f841320c0301f5e1a76f2d0b8ee42c8b468fc
Dalal Alrayes <dalrayes@gmail.com>
2015-04-16 22:13:45 -0500

    Added landing page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
 
   resources :cards
   
-  root 'pages#home'
+  root 'static_pages#home'
+  get '/home' => 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
