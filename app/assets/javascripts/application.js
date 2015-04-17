ace8b4f444aa5b6737b31c9bb5098a9efd3799e0
Dalal Alrayes <dalrayes@gmail.com>
2015-04-16 20:11:21 -0500

    Flashcard flip working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
-//= require turbolinks
 //= require_tree .
 
 
-$(function(){ $(document).foundation(); });
+$(document).ready(function() {
+	$('.flashcard').on('click',function() {
+		$('.flashcard').toggleClass('flipped');
+	});
+});
